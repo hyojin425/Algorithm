8a08cee58c7796ed550df2f706e29ff35ba48630
hyojin <gywls5184@gmail.com>
2024-06-02 18:03:38 +0900

    [Silver V] Title: 다리 놓기, Time: 152 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,26 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                cnt = cnt * (m - j) / (j + 1);
+            }
+            System.out.println(cnt);
+        }
+    }
+}
