481bdda93009a8c7342d0ce03f10cc770c8228c4
hyojin <gywls5184@gmail.com>
2024-06-10 23:57:12 +0900

    [level 2] Title: 짝지어 제거하기, Time: 47.36 ms, Memory: 59 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = -1;
+        Stack<Character> stack = new Stack<>();
+    
+        for(int i=0; i<s.length(); i++){
+            if(stack.isEmpty()) stack.push(s.charAt(i));
+            else {
+                char ch = s.charAt(i); 
+
+                if (stack.peek() == ch) {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+        
+        return stack.isEmpty() ? 1:0;
+    }
+}
