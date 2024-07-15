e353582bfd0626e31faf29e800b0a9512c5046fe
hyojin <gywls5184@gmail.com>
2024-07-15 23:37:59 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,30 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[101][101];  // 도화지
+        int size = 0; // 색종이 넓이
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        size++;
+                    }
+
+                }
+            }
+        }
+        System.out.println(size);
+    }
+}
