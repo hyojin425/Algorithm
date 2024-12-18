6a4a468f7864436b73cc672cfcf44b93bfab7990
hyojin <gywls5184@gmail.com>
2024-12-19 01:01:57 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,27 @@
+n, r, c = map(int, input().split())
+
+def dfs(r, c, size, n):
+    size = size // 2
+    if r < size and c < size:
+        if size == 1:
+            print(n)
+            exit()
+        dfs(r, c, size, n)
+    elif r < size and c >= size:
+        if size == 1:
+            print(n + 1)
+            exit()
+        dfs(r, c - size, size, n + size ** 2)
+    elif r >= size and c < size:
+        if size == 1:
+            print(n + 2)
+            exit()
+        dfs(r - size, c, size, n + (size ** 2) * 2)
+    elif r >= size and c >= size:
+        if size == 1:
+            print(n + 3)
+            exit()
+        dfs(r - size, c - size, size, n + (size ** 2) * 3)
+    
+        
+dfs(r, c, 2 ** n, 0)
