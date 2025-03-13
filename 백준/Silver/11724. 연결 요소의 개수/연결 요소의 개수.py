bd991a0db9508f306548b967bfb8062fe9369d2f
hyojin <gywls5184@gmail.com>
2025-03-13 13:01:26 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 656 ms, Memory: 67228 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,19 +1,15 @@
 import sys
+sys.setrecursionlimit(10**6)
 from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())    
     
-def bfs(start, arr, visited):
-    q = deque([start])
-    visited[start] = 1
-    
-    while q:
-        v = q.popleft()
-        for i in arr[v]:
-            if visited[i] != 1:
-                q.append(i)
-                visited[i] = 1
+def dfs(v, arr, visited):
+    visited[v] = 1
+    for i in arr[v]:
+        if visited[i] == 0:
+            dfs(i, arr, visited)
   
 result = 0              
 arr = [[] for _ in range(n + 1)]
@@ -25,8 +21,8 @@ for _ in range(m):
     arr[b].append(a)
     
 for i in range(1, n + 1):
-    if visited[i] != 1:
-        bfs(i, arr, visited)
+    if visited[i] == 0:
+        dfs(i, arr, visited)
         result += 1
     
 print(result)
