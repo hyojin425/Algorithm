52d8dd21f50b0e1b45b606546e38ee17dea07276
hyojin <gywls5184@gmail.com>
2025-07-08 20:53:59 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,34 +1,18 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.math.BigInteger;
+
 
 public class Main {
-
-    static ArrayList<Integer> arr;
-    public static void DFS(int n, int b){
-        if(n<b) {
-            arr.add(n);
-        }
-        else {
-            DFS(n/b,b);
-            arr.add(n % b);
-        }
-    }
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int n = Integer.parseInt(str.split(" ")[0]);
-        int b = Integer.parseInt(str.split(" ")[1]);
 
-        arr = new ArrayList<>();
-        DFS(n, b);
-        String answer = "";
-        for(int x:arr) {
-            if(x>=10) answer += Character.toString(x + 55);
-            else answer+=x;
-        }
+        String[] arr = br.readLine().split(" ");
 
-        System.out.print(answer);
+        BigInteger n = new BigInteger(arr[0]);
+
+        String result = n.toString(Integer.parseInt(arr[1]));
+
+        System.out.println(result.toUpperCase());
     }
 }
