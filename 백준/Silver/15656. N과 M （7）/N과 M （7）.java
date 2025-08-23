b60d6cd8ef7d5c7108dc972d6f5ad5ddd0eaa0fd
hyojin <gywls5184@gmail.com>
2025-08-23 23:15:15 +0900

    [Silver III] Title: N과 M (7), Time: 664 ms, Memory: 126876 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] pm;
+    static StringBuilder sb = new StringBuilder();
+    public  void DFS(int l){
+        if(l == m) {
+            for(int i = 0; i< m; i++){
+                sb.append(pm[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for( int i = 0; i< n; i++){
+                pm[l] = arr[i];
+                DFS(l + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main t = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        n  = scanner.nextInt();
+        m = scanner.nextInt();
+        arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        pm = new int[n];
+        t.DFS(0);
+        System.out.print(sb.toString());
+    }
+
+}
