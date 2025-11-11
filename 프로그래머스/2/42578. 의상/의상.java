2abb6cbbe16863124eb6bfe47fb5982e0bb7cac3
hyojin <gywls5184@gmail.com>
2025-11-11 15:48:50 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,21 +1,18 @@
 import java.util.*;
 class Solution {
 public int solution(String[][] clothes) {
-        int answer = 1;
-
+    int answer = 1;
         HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+    
+        for(String[] c: clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
         }
-
-        Iterator<Integer> iter = map.values().iterator();
-        while(iter.hasNext()){
-            answer *= iter.next().intValue() +1;
+    
+        for(int v: map.values()) {
+            answer *= (v + 1);
         }
-
-
-
-        return answer -1 ;
+    
+    return answer - 1;
 
     }
 }
