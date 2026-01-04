2d51709b292e45cdf0796ba1757e31633738ef25
hyojin <gywls5184@gmail.com>
2026-01-05 03:24:49 +0900

    [Silver II] Title: 랜선 자르기, Time: 172 ms, Memory: 18040 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static long solution(int m, int[] arr, int n) {
+        Arrays.sort(arr);
+        long min = 1;
+        long max = arr[n - 1];
+        long answer = 0;
+        while(min <= max) {
+            long mid = (min + max) / 2;
+            long cnt = getCnt(mid, arr);
+            if(cnt >= m) {
+                answer = mid;
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    public static long getCnt(long len, int[] arr) {
+        long cnt = 0;
+        for (int a : arr) {
+            cnt += a / len;
+        }
+        return cnt;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] nums = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int n = nums[0];
+        int m = nums[1];
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println(solution(m, arr, n));
+    }
+}
