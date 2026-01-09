45265eea6069de1d808e8fbc62145d705d33dc9e
hyojin <gywls5184@gmail.com>
2026-01-10 00:24:52 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+public class Main {
+
+    public static void solution(int n, int[] arr) {
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if(arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for(int i = 0; i <n; i++){
+            max = Math.max(dp[i], max);
+        }
+        System.out.println(n - max);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+//
+//
+        solution(n, arr);
+
+    }
+}
