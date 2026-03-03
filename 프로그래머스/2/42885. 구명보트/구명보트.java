674fd6c182c81e6cfcaa9dfff0c030f7e0a0dcbc
hyojin <gywls5184@gmail.com>
2026-03-03 18:27:02 +0900

    [level 2] Title: 구명보트, Time: 11.17 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,19 +4,18 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int len = people.length;
-        int[] ch = new int[len];
-        int lt = 0, rt = len - 1;
         
-        while(lt <= rt){
-            if(limit >= people[rt] + people[lt]){
-                answer++;
+        int rt = people.length - 1;
+        int lt = 0;
+        
+        while(lt <= rt) {
+            if(people[lt] + people[rt] <= limit) {
                 lt++;
                 rt--;
-            }else {
-                answer++;
+            } else if(limit - people[lt] < people[rt]) {
                 rt--;
             }
+            answer++;
         }
         
         return answer;
