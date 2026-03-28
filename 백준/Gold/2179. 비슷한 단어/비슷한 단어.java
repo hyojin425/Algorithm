44ee315807ab1195d68c4a517c924b95d2b6157e
hyojin <gywls5184@gmail.com>
2026-03-28 14:51:38 +0900

    [Gold III] Title: 비슷한 단어, Time: 680 ms, Memory: 18052 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -1,49 +1,49 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        
-        String[] original = new String[n];
-        for (int i = 0; i < n; i++) {
-            original[i] = br.readLine();
-        }
+// The main method must be in a class named "Main".
+class Main {
 
-        int maxLen = -1;
-        String s1 = "", s2 = "";
+    public static void solution(int n, String[] arr) {
+        int max = Integer.MIN_VALUE;
+        String[] answer = new String[2];
+        for(int i =0 ; i < n; i++) {
+            String cur = arr[i];
+            for(int j = i + 1; j < n; j++) {
+                String t = arr[j];
 
-        // 모든 단어 쌍을 비교하면 O(N^2 * L)로 위험하지만, 
-        // 문제의 N과 제한 시간을 고려할 때 접두사 기준으로 그룹화하면 효율적입니다.
-        for (int i = 0; i < n; i++) {
-            String target = original[i];
-            
-            for (int j = i + 1; j < n; j++) {
-                String compare = original[j];
+                int len = Math.min(cur.length(), t.length());
+
+                if(len <= max) continue;
                 
-                // 이미 찾은 최대 길이보다 짧은 단어는 스킵 (최적화)
-                if (target.equals(compare)) continue; 
-
-                int count = getCommonPrefixLength(target, compare);
-
-                if (count > maxLen) {
-                    maxLen = count;
-                    s1 = target;
-                    s2 = compare;
+                int cnt = 0;
+                for(int k = 0; k < len; k++) {
+                    if(cur.charAt(k) == t.charAt(k)) cnt++;
+                    else break;
+                }
+                
+                if(cnt > max) {
+                    max = cnt;
+                    answer[0] = cur;
+                    answer[1] = t;
                 }
             }
         }
-
-        System.out.println(s1);
-        System.out.println(s2);
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
+        
     }
 
-    private static int getCommonPrefixLength(String a, String b) {
-        int len = Math.min(a.length(), b.length());
-        for (int i = 0; i < len; i++) {
-            if (a.charAt(i) != b.charAt(i)) return i;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
         }
-        return len;
+        
+        solution(n, arr);
     }
 }
