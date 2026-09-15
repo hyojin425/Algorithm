d02d788e32be060eb79aa9e11f719dfb68e73ba2
hyojin <gywls5184@gmail.com>
2026-09-15 18:40:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.80 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new  HashMap<>();
+        for(String p: participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        for(String c: completion) {
+            if(map.get(c) == 1) {
+                map.remove(c);
+            } else {
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        for(Map.Entry<String, Integer> e : map.entrySet()) {
+            String s = e.getKey();
+            Integer i = e.getValue();
+            return s;
+        }
+        String answer = "";
+        return answer;
+    }
+}
