0ba98b7bafaf7311ac85eb0f135c5c9fe2a59c8c
hyojin <gywls5184@gmail.com>
2026-09-15 21:52:11 +0900

    [level 3] Title: 아이템 줍기, Time: 1.27 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,53 +1,59 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        
-        int n = rectangle.length;
-        int[][] map = new int[102][102];
-        for(int i = 0; i < n; i++) {
-            int[] tmp = rectangle[i];
-            for(int j = tmp[0] * 2; j <= tmp[2] * 2; j++) {
-                for(int k = tmp[1] * 2; k <= tmp[3] * 2; k++) {
-                    map[j][k] = 1;
+        int[][] map = new int[101][101];
+        for(int[] r: rectangle) {
+            int x1 = r[0];
+            int y1 = r[1];
+            int x2 = r[2];
+            int y2 = r[3];
+            for(int i = x1 * 2; i <= x2 * 2; i++) {
+                for(int j = y1 * 2; j <= y2 * 2; j++) {
+                    map[i][j] = 1;
                 }
             }
         }
         
-        for(int i = 0; i < n; i++) {
-            int[] tmp = rectangle[i];
-            for(int j = tmp[0] * 2 + 1; j < tmp[2] * 2; j++) {
-                for(int k = tmp[1] * 2 + 1; k < tmp[3] * 2; k++) {
-                    map[j][k] = 0;
+        for(int[] r: rectangle) {
+            int x1 = r[0];
+            int y1 = r[1];
+            int x2 = r[2];
+            int y2 = r[3];
+            for(int i = x1 * 2 + 1; i < x2 * 2; i++) {
+                for(int j = y1 * 2 + 1; j < y2 * 2; j++) {
+                    map[i][j] = 0;
                 }
             }
         }
         
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{characterX * 2, characterY * 2});
-        int[][] dis = new int[102][102];
-        dis[characterX * 2][characterY * 2] = 1;
-        
-        int[] dx = {0, -1, 0, 1};
-        int[] dy = {1, 0, -1, 0};
-        
+        map[characterX *2][characterY * 2] = 0;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {-1, 0, 1, 0};
+        int dis = 0;
         while(!q.isEmpty()) {
-            int[] tmp = q.poll();
-            int x = tmp[0];
-            int y = tmp[1];
-            for(int i =0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx == itemX * 2 && ny == itemY * 2) return dis[x][y] / 2;
-                
-                if(nx >= 0 && nx < 102 && ny >= 0 && ny < 102 
-                  && map[nx][ny] == 1 && dis[nx][ny] == 0) {
-                    q.add(new int[]{nx, ny});
-                    dis[nx][ny] = dis[x][y] + 1;
+            int len = q.size();
+            
+            for(int j = 0; j < len; j++) {
+                int[] tmp = q.poll();
+                if(tmp[0] == itemX * 2 && tmp[1] == itemY * 2) {
+                    return dis / 2;
+                }
+                for(int i = 0; i < 4; i++) {
+                    int nx = tmp[0] + dx[i];
+                    int ny = tmp[1] + dy[i];
+                    if(nx <= 100 && nx >= 0 && ny <= 100 && ny >= 0 && 
+                       map[nx][ny] == 1) {
+                        q.add(new int[]{nx, ny});
+                        map[nx][ny] = 0;
+                    }
                 }
             }
+            dis++;
         }
-        int answer = 0;
-        return answer;
+        return 0;
     }
+    
+    
 }
